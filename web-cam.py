4abcef9219ed867426d17a07a792f576d98434a1
sardor <alimukhammad1@gmail.com>
2024-04-24 23:32:41 -0400

    2.1

--- web-cam.py
+++ web-cam.py
@@ -1,10 +1,10 @@
 import cv2
+import easyocr
 
 # Open the webcam (0 for default camera, 1 for the second camera, etc.)
 cam = cv2.VideoCapture(0)
 
 frame_counter = 0
-
 result, image = cam.read()
 
 while True:
@@ -19,9 +19,22 @@ while True:
 
     # Save the frame on 'c' key press
     if key == ord('c'):
+        # save image to variable
+        image = frame.copy()
+
         cv2.imwrite('frame_{}.png'.format(frame_counter), frame)
         frame_counter += 1
         
+    # 
+        reader = easyocr.Reader(['en'])
+        results = reader.readtext(image)
+
+        # print the extracted text to the console
+        for result in results:
+             print(result[1])
+    #
+    # needs work to display the text on the image
+      
     # Break the loop on 'q' key press
     elif key == ord('q'):    
         break
