35adca9dc03890eddaa5ba8053c4f998f313be57
sardor <alimukhammad1@gmail.com>
2024-05-25 18:12:42 -0400

    matlab

--- read-img.py
+++ read-img.py
@@ -1,23 +1,9 @@
-# Importing the OpenCV library
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Reading the image using imread() function
-image = cv2.imread('image.jpg')
+img = cv2.imread("image.jpg")
 
-# We are copying the original image,
-# as it is an in-place operation.
-output = image.copy()
-
-# Using the rectangle() function to create a rectangle.
-text = cv2.putText(output, 'Sardor great', (100, 250),
-                        cv2.FONT_HERSHEY_SIMPLEX, 4, (100, 0, 0), 2)
-
-# # Resizing the images
-# image_resized = cv2.resize(image, (800, 600))
-# rectangle_resized = cv2.resize(rectangle, (800, 600))
-
-# Displaying the image with rectangle
-cv2.imshow("Image with Rectangle", text)
-cv2.waitKey(0)
-
-
+plt.imshow(img)
+plt.waitforbuttonpress()
+plt.close('all')
